38ce1a269c7d2dd9b2c4c5ffc9f6d4ffdcf90541
That's Courtney <courtney@thatscourtney.com>
2024-01-21 14:09:55 -0600

    Create patience.py
    fun little exercise from CodeAcademy's Control Flow review

---
+++ patience.py
@@ -0,0 +1,16 @@
+# patience = 0 #
+patience = 1.25 #I have some patience
+
+
+#check if safe to approach me
+if patience <= 0:
+  print("Honey, I got you some chocolate chip cookies and I'm running you a warm shower after you devour them.")
+
+elif patience > 1:
+  print("You look great today, but I think we should wait for this thing to happen.")
+
+else: 
+    print("Whatever.")
+
+
+

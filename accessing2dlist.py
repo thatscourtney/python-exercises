62dc8c8f1814effc7eacf6e45b43e3226e7d3f08
That's Courtney <courtney@thatscourtney.com>
2024-01-28 11:27:36 -0600

    Create accessing2dlist.py
    can use neg and pos indices together

---
+++ accessing2dlist.py
@@ -0,0 +1,15 @@
+#Your code below:
+class_name_test = [["Jenny", 90], ["Alexus", 85.5], ["Sam", 83], ["Ellie", 101.5]]
+print(class_name_test)
+
+#select Sam's test score
+sams_score = class_name_test[2][1]
+print(sams_score)
+
+#select Ellie's test score
+ellies_score = class_name_test[-1][-1]
+print(ellies_score)
+
+#test to see if I can combine neg. and pos
+jennys_score = class_name_test[0][-1]
+print(jennys_score)
